aac49d044b131f0130dd1ad7aa75b80b21c16408
Quixotic95 <e.oguz95@hotmail.com>
2021-08-24 22:09:33 +0300

    unused imports deleted

--- third-homework/src/main/java/dev/patika/quixotic95/thirdhomework/entity/Person.java
+++ third-homework/src/main/java/dev/patika/quixotic95/thirdhomework/entity/Person.java
@@ -5,9 +5,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
