a97ea0ba4c69d56002b3e8021aa401c5af597711
Quixotic95 <e.oguz95@hotmail.com>
2021-08-24 17:40:00 +0300

    First Commit
    - .gitignore added for IDEA files/folders
    
    - spring initialized to project
    
    - h2 database connection properties added to application.properties

---
+++ third-homework/src/main/java/dev/patika/quixotic95/thirdhomework/ThirdHomeworkApplication.java
@@ -0,0 +1,13 @@
+package dev.patika.quixotic95.thirdhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThirdHomeworkApplication.class, args);
+    }
+
+}
---
+++ third-homework/src/test/java/dev/patika/quixotic95/thirdhomework/ThirdHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.patika.quixotic95.thirdhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
