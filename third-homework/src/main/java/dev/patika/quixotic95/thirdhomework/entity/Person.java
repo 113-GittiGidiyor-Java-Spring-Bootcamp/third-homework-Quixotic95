4768bb78e7400e656679eca84551ef3430f47d2c
Quixotic95 <e.oguz95@hotmail.com>
2021-08-28 02:35:30 +0300

    NoNNull annotation from lombok added

--- third-homework/src/main/java/dev/patika/quixotic95/thirdhomework/entity/Person.java
+++ third-homework/src/main/java/dev/patika/quixotic95/thirdhomework/entity/Person.java
@@ -3,6 +3,7 @@ package dev.patika.quixotic95.thirdhomework.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import lombok.experimental.SuperBuilder;
 
 import javax.persistence.MappedSuperclass;
@@ -14,8 +15,11 @@ import javax.persistence.MappedSuperclass;
 @SuperBuilder
 public abstract class Person {
 
+    @NonNull
     private String firstName;
+    @NonNull
     private String lastName;
+    @NonNull
     private String address;
 
 }
