6ed13ab5835a0cd74485df41d5b7802d4d4a123a
Quixotic95 <e.oguz95@hotmail.com>
2021-08-28 02:36:28 +0300

    student repository added

--- third-homework/src/main/java/dev/patika/quixotic95/thirdhomework/repository/StudentRepository.java
+++ third-homework/src/main/java/dev/patika/quixotic95/thirdhomework/repository/StudentRepository.java
@@ -1,10 +1,21 @@
 package dev.patika.quixotic95.thirdhomework.repository;
 
 import dev.patika.quixotic95.thirdhomework.entity.Student;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface StudentRepository extends CrudRepository<Student, Integer> {
+
+    List<Student> findStudentsByFirstNameAndLastName(String firstName, String lastName);
+
+    @Query("select s.gender, count(s.gender) from Student s GROUP BY s.gender")
+    List<?> getStudentsGendersWithGrouping();
+
+    void deleteStudentByFirstNameAndLastName(String firstName, String lastName);
 
 }
