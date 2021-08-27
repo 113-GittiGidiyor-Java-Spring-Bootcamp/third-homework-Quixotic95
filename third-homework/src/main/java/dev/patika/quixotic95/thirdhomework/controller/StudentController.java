e627d8f6e054f57afcda51564fceafd9c7164316
Quixotic95 <e.oguz95@hotmail.com>
2021-08-28 02:38:57 +0300

    ***student controller added***
    -> findStudentsByFirstNameAndLastName method returns list, because deleteStudentByFirstNameAndLastName method calls it and gets the value.
    
    TO-DO
    -> for current project, Response Entities will be removed. And the other controllers also created withour ResponseEntity.

---
+++ third-homework/src/main/java/dev/patika/quixotic95/thirdhomework/controller/StudentController.java
@@ -0,0 +1,107 @@
+package dev.patika.quixotic95.thirdhomework.controller;
+
+import dev.patika.quixotic95.thirdhomework.entity.Student;
+import dev.patika.quixotic95.thirdhomework.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    // dependency injection with @Autowired annotation (not necessary to write, injects automatically; but placed for better-reading)
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    // expose "/students" and return list of students
+    @GetMapping("/students")
+    public ResponseEntity<?> findAllStudents() {
+        return new ResponseEntity<>(studentService.findAllStudents(), HttpStatus.OK);
+    }
+
+    // add mapping for GET /students/{studentId} to get a student by id
+    @GetMapping("/students/{studentId}")
+    public ResponseEntity<?> findStudentById(@PathVariable int studentId) {
+        Optional<Student> foundStudent = studentService.findStudentById(studentId);
+        if (foundStudent.isPresent()) {
+            return new ResponseEntity<>(foundStudent, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Student with id: " + studentId + " not found.", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    // add mapping for POST /students - add new student
+    @PostMapping("/students")
+    public ResponseEntity<?> saveStudent(@RequestBody Student student) {
+        int studentId = student.getId();
+        if (studentService.existsById(studentId)) {
+            return new ResponseEntity<>("Student with id: " + studentId + " already exists.", HttpStatus.BAD_REQUEST);
+        } else {
+            return new ResponseEntity<>(studentService.saveStudent(student), HttpStatus.OK);
+        }
+    }
+
+    // add mapping for PUT /students - update existing student
+    @PutMapping("/students")
+    public ResponseEntity<?> updateStudent(@RequestBody Student student) {
+        if (studentService.existsById(student.getId())) {
+            return new ResponseEntity<>(studentService.saveStudent(student), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Student with id: " + student.getId() + " not found.", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    // add mapping for DELETE /students - delete student
+    @DeleteMapping("/students")
+    public ResponseEntity<String> deleteStudent(@RequestBody Student student) {
+        int studentId = student.getId();
+        if (studentService.existsById(studentId)) {
+            studentService.deleteStudent(student);
+            return new ResponseEntity<>("Student with id: " + studentId + " deleted", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Student with id: " + studentId + " not found.", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    // add mapping for DELETE /students/{studentId} - delete student by id
+    @DeleteMapping("/students/{studentId}")
+    public ResponseEntity<String> deleteStudent(@PathVariable int studentId) {
+        if (studentService.existsById(studentId)) {
+            studentService.deleteStudentById(studentId);
+            return new ResponseEntity<>("Student with id: " + studentId + " deleted", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Student with id: " + studentId + " not found.", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    // add mapping for GET student(s) by their name
+    @GetMapping("/students/byName")
+    public List<Student> findStudentsByFirstNameAndLastName(@RequestParam String firstName, @RequestParam String lastName) {
+        return studentService.findStudentsByFirstNameAndLastName(firstName, lastName);
+    }
+
+    @GetMapping("/students/groupByGender")
+    public ResponseEntity<?> getStudentsGendersWithGrouping() {
+        return new ResponseEntity<>(studentService.getStudentsGendersWithGrouping(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/students/byName")
+    public ResponseEntity<String> deleteStudentByFirstNameAndLastName(@RequestParam String firstName, @RequestParam String lastName) {
+        if (findStudentsByFirstNameAndLastName(firstName, lastName).isEmpty()) {
+            return new ResponseEntity<>("Student with name: " + firstName + " " + lastName + " not found.", HttpStatus.BAD_REQUEST);
+        } else {
+            studentService.deleteStudentByFirstNameAndLastName(firstName, lastName);
+            return new ResponseEntity<>("Student with name: " + firstName + " " + lastName + " deleted.", HttpStatus.OK);
+        }
+    }
+
+}
