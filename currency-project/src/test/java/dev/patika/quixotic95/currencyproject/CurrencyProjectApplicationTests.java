608467dc7ee2d753f11a0a93d6b240439cee2260
Quixotic95 <e.oguz95@hotmail.com>
2021-08-29 19:33:33 +0300

    currency project added

---
+++ currency-project/src/test/java/dev/patika/quixotic95/currencyproject/CurrencyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.patika.quixotic95.currencyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
